b7d7e95557b7dee668da5feef80e7d15bbce63ac
Kimseohyeon <seohyeon0219@naver.com>
2025-02-26 17:36:48 +0900

    v1.4 day13 ml assignment

--- day13.py
+++ day13.py
@@ -1,3 +1,7 @@
+# Assignment
+# 데이터 로딩 -> 데이터 전처리 -> 타겟 및 독립변수 설정 -> 트레이닝 / 테스트 셋 설정
+# -> 모델 선택 및 학습 -> 에측 수행 -> 성능 평가 (mse 등) -> 시각화
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -7,3 +11,6 @@ from sklearn.model_selection import train_test_split
 
 mpg = sns.load_dataset('mpg')
 print(mpg.info())
+print(mpg[["horsepower"]].tail(10))
+print(mpg.tall(10))
+
